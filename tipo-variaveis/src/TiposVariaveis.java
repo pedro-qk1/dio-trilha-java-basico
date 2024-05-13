6052fa2784c3da2393fcbb6bcbfad8dc812a74cb
pedro-qk1 <pedrovictor2003@gmail.com>
2024-05-13 15:44:27 -0300

    parte 2 dos tipos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500; // no caso do double, só é necessario colocar o "." caso o numero tenha casa decimal, por exemplo, "2.83"
+
+        final double VALOR_DE_PI = 3.14; // toda variavel constante deverá ser escrita em CAIXA ALTA e ter a palavra "final" antes do tipo da variavel
     }
 }
