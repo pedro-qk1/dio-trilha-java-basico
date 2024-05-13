c1b50c014f23710fdb6a7308bfe70f151542cbd7
pedro-qk1 <pedrovictor2003@gmail.com>
2024-05-13 16:12:36 -0300

    feat: adiciona novos programas

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args){
+        // operador ternario
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "Verdadeiro!" : "Falso!";
+        System.out.println(resultado);
+
+        // String resultado = ""
+        // if (a == b){
+        //     System.out.println("Verdareiro!"); 
+        // } else{
+        //     System.out.println("Falso!");
+        // }
+    }
+}
--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -3,5 +3,8 @@ public class TiposVariaveis {
         double salarioMinimo = 2500; // no caso do double, só é necessario colocar o "." caso o numero tenha casa decimal, por exemplo, "2.83"
 
         final double VALOR_DE_PI = 3.14; // toda variavel constante deverá ser escrita em CAIXA ALTA e ter a palavra "final" antes do tipo da variavel
+
+        System.out.println(salarioMinimo);
+        System.out.println(VALOR_DE_PI);
     }
 }
